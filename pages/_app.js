f4d3c750c3b8b249491dd79ca6c2ba4e8d83f5da
Angel Reyes <angelmrg2000@hotmail.com>
2020-07-30 17:10:39 -0600

    pages/_app: added some divs

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
-import NavBar from "../components/Navbar/navbar.component";
-import Footer from "../components/Footer/Footer.component";
-import "./global.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Footer from "../components/Footer/Footer.component";
+import NavBar from "../components/Navbar/navbar.component";
+import "./global.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <NavBar />
-      <Component {...pageProps} />
-      <Footer />
+      <div className="layout">
+        <div className="child1">
+          <Component {...pageProps} />
+        </div>
+
+        <div className="child2">
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
